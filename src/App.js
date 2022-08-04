d0901185acdc2ea79fc9ea96e597f167403fc678
Collins Kiprono <one.kcollin@gmail.com>
2022-08-04 17:23:56 +0300

    Setup Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Let's get started!</h1>
+      <h1 className="text-3xl font-bold underline">Let's get started!</h1>
       <Expenses items={expenses} />
     </div>
   );
